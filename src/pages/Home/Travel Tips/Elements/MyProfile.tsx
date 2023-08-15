a682cd2e32671894ff9d960fb864e98a1ebb33f8
rezzak ali <rezzak ali>
2023-08-15 12:38:16 +0530

    code push

--- src/pages/Home/Travel Tips/Elements/MyProfile.tsx
+++ src/pages/Home/Travel Tips/Elements/MyProfile.tsx
@@ -9,7 +9,7 @@ import SocialMedia from '@/ui/SocialMedia';
 
 const MyProfile = () => {
   return (
-    <Card className="mt-6 overflow-hidden dark:bg-transparent border dark:border-gray-700 rounded transition duration-300 ease-in hover:-translate-y-1 hover:ease-in">
+    <Card className="mt-6 overflow-hidden dark:bg-transparent border dark:border-gray-700 rounded transition duration-300 ease-in hover:-translate-y-1 hover:ease-in space-y-8">
       <CardBody>
         <Avatar
           src={`/assets/team.jpg`}
@@ -18,16 +18,16 @@ const MyProfile = () => {
           alt="profile pic"
           className="object-cover w-16 h-16 rounded-full"
         />
-        <Typography variant="h5" className="mb-2">
+        <Typography variant="h5" className="mb-2 text-textcolor">
           Hello, I am Steven
         </Typography>
-        <Typography>
+        <Typography className="text-textcolor">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, sit
           possimus qui vitae fuga quam fugit assumenda ab aliquid libero!
         </Typography>
       </CardBody>
-      <CardFooter className="pt-0">
-        Follow me : <SocialMedia />
+      <CardFooter className="pt-0 text-white flex items-center gap-3">
+        <Typography> Follow me : </Typography> <SocialMedia />
       </CardFooter>
     </Card>
   );
--- src/pages/Home/Travel Tips/TravelTips.tsx
+++ src/pages/Home/Travel Tips/TravelTips.tsx
@@ -9,7 +9,7 @@ const TravelTips = () => {
       <div className="flex sm:flex-col md:flex-row lg:flex-row item-center justify-between w-full gap-3">
         <div className="lg:w-2/3 md:w-2/3 sm:w-full my-8">
           <PostCategoryHeader text={`Travel Tips`} />
-          <hr className="text-gray-100 dark:text-gray-700 my-8" />
+          <hr className="text-gray-100 dark:text-gray-700 my-8 bg-transparent" />
           <div
             data-aos="fade-up"
             className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3"
